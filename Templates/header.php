79a992a16d9dc50784da749e554ed2b4c2beb7a1
Davi Júlio <davijulio550@gmail.com>
2023-11-11 20:15:33 -0300

    Modificação de pastas

--- Templates/header.php
+++ Templates/header.php
@@ -13,8 +13,8 @@ include_once "Config/config.php";
     <title>Home</title>
     <link rel="short icon" href="<?=$BASE_URL?>/Imgs/agenda.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= $BASE_URL?>/Css/style.css">
-    <link rel="stylesheet" href="<?= $BASE_URL?>/Css/mediaQuery.css">
+    <link rel="stylesheet" href="<?= $BASE_URL?>/Views/Css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL?>/Views/Css/mediaQuery.css">
 </head>
 <body>
 
@@ -31,7 +31,6 @@ include_once "Config/config.php";
 </header><!--header-->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
-    
+<script src="<?=$BASE_URL?>"/Views/JS/script.js></script>
 </body>
 </html>
